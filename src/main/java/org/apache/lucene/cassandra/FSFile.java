675d99bb07fa0b6545560b4164212575cd170208
Jason Wee <peichieh@gmail.com>
2014-04-30 14:04:05 +0800

    OTHER-932 commit work done by eddie: added getRandomAccessFile

--- src/main/java/org/apache/lucene/cassandra/FSFile.java
+++ src/main/java/org/apache/lucene/cassandra/FSFile.java
@@ -18,6 +18,7 @@ package org.apache.lucene.cassandra;
  */
 
 //import java.util.ArrayList;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.Arrays;
@@ -27,126 +28,100 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /** @lucene.internal */
-public class FSFile  implements Serializable {
+public class FSFile implements File, Serializable {
 
     private static final long serialVersionUID = 1l;
 
     java.io.File thePrivateFile;
-//    CassandraFile thePrivateFile;
-/*
 
-[nejoom:~/Documents/workspace/luceneOnCassandra]$ cat stats | cut -c 21- | sort | uniq -c | sort -n
-   2 File [TRACE] File(String canonicalPath
-   2 File [TRACE] getCanonicalPath()
-   2 File [TRACE] list()
-   4 File [TRACE] isDirectory()
-   5 RandomAccessFile [TRACE] getFDsync()
-  14 RandomAccessFile [TRACE] read()
-  14 RandomAccessFile [TRACE] seek()
-  14 RandomAccessFile [TRACE] write()
-  15 File [TRACE] getPath
-  15 RandomAccessFile [TRACE] length()
-  16 File [TRACE] delete()
-  19 RandomAccessFile [TRACE] close()
-  21 File [TRACE] length()
-  35 File [TRACE] exists()
-  35 RandomAccessFile [TRACE] RandomAccessFile(File path, String string)
-  85 File [TRACE] File(File dir, String file)
- 120 File [TRACE] java.io.File getFile()
-
- [nejoom:~/Documents/workspace/luceneOnCassandra]$ cat stats.write.bigger | cut -c 21- | sort | uniq -c | sort -n
-   2 File [TRACE] File(String canonicalPath
-   2 File [TRACE] getCanonicalPath()
-   2 File [TRACE] list()
-   4 File [TRACE] isDirectory()
-   5 RandomAccessFile [TRACE] getFDsync()
-  14 RandomAccessFile [TRACE] read()
-  14 RandomAccessFile [TRACE] seek()
-  14 RandomAccessFile [TRACE] write()
-  15 File [TRACE] getPath
-  15 RandomAccessFile [TRACE] length()
-  16 File [TRACE] delete()
-  19 RandomAccessFile [TRACE] close()
-  21 File [TRACE] length()
-  35 File [TRACE] exists()
-  35 RandomAccessFile [TRACE] RandomAccessFile(File path, String string)
-  85 File [TRACE] File(File dir, String file)
- 120 File [TRACE] java.io.File getFile()
-
- 
- [nejoom:~/Documents/workspace/luceneOnCassandra]$ cat stats.read | cut -c 21- | sort | uniq -c | sort -n
-   1 File [TRACE] list()
-   1 uments
-   2 File [TRACE] File(String canonicalPath
-   2 File [TRACE] exists()
-   2 File [TRACE] getCanonicalPath()
-   2 File [TRACE] isDirectory()
-   3 
-   7 File [TRACE] File(File dir, String file)
-   7 RandomAccessFile [TRACE] RandomAccessFile(File path, String string)
-   7 RandomAccessFile [TRACE] close()
-   7 RandomAccessFile [TRACE] length()
-  14 File [TRACE] java.io.File getFile()
-  16 File [TRACE] getPath
-  22 RandomAccessFile [TRACE] read()
-  22 RandomAccessFile [TRACE] seek()
-
-
-[nejoom:~/Documents/workspace/luceneOnCassandra]$ cat stats.read.bigger | cut -c 21- | sort | uniq -c | sort -n
-   1 File [TRACE] list()
-   1 uments
-   2 File [TRACE] File(String canonicalPath
-   2 File [TRACE] exists()
-   2 File [TRACE] getCanonicalPath()
-   2 File [TRACE] isDirectory()
-   5 
-   7 File [TRACE] File(File dir, String file)
-   7 RandomAccessFile [TRACE] RandomAccessFile(File path, String string)
-   7 RandomAccessFile [TRACE] close()
-   7 RandomAccessFile [TRACE] length()
-  14 File [TRACE] java.io.File getFile()
-  16 File [TRACE] getPath
-  24 RandomAccessFile [TRACE] read()
-  24 RandomAccessFile [TRACE] seek()
-
-
-
- */
-private static Logger logger = LoggerFactory
-.getLogger(File.class);
+    // CassandraFile thePrivateFile;
+    /*
+     * 
+     * [nejoom:~/Documents/workspace/luceneOnCassandra]$ cat stats | cut -c 21-
+     * | sort | uniq -c | sort -n 2 File [TRACE] File(String canonicalPath 2
+     * File [TRACE] getCanonicalPath() 2 File [TRACE] list() 4 File [TRACE]
+     * isDirectory() 5 RandomAccessFile [TRACE] getFDsync() 14 RandomAccessFile
+     * [TRACE] read() 14 RandomAccessFile [TRACE] seek() 14 RandomAccessFile
+     * [TRACE] write() 15 File [TRACE] getPath 15 RandomAccessFile [TRACE]
+     * length() 16 File [TRACE] delete() 19 RandomAccessFile [TRACE] close() 21
+     * File [TRACE] length() 35 File [TRACE] exists() 35 RandomAccessFile
+     * [TRACE] RandomAccessFile(File path, String string) 85 File [TRACE]
+     * File(File dir, String file) 120 File [TRACE] java.io.File getFile()
+     * 
+     * [nejoom:~/Documents/workspace/luceneOnCassandra]$ cat stats.write.bigger
+     * | cut -c 21- | sort | uniq -c | sort -n 2 File [TRACE] File(String
+     * canonicalPath 2 File [TRACE] getCanonicalPath() 2 File [TRACE] list() 4
+     * File [TRACE] isDirectory() 5 RandomAccessFile [TRACE] getFDsync() 14
+     * RandomAccessFile [TRACE] read() 14 RandomAccessFile [TRACE] seek() 14
+     * RandomAccessFile [TRACE] write() 15 File [TRACE] getPath 15
+     * RandomAccessFile [TRACE] length() 16 File [TRACE] delete() 19
+     * RandomAccessFile [TRACE] close() 21 File [TRACE] length() 35 File [TRACE]
+     * exists() 35 RandomAccessFile [TRACE] RandomAccessFile(File path, String
+     * string) 85 File [TRACE] File(File dir, String file) 120 File [TRACE]
+     * java.io.File getFile()
+     * 
+     * 
+     * [nejoom:~/Documents/workspace/luceneOnCassandra]$ cat stats.read | cut -c
+     * 21- | sort | uniq -c | sort -n 1 File [TRACE] list() 1 uments 2 File
+     * [TRACE] File(String canonicalPath 2 File [TRACE] exists() 2 File [TRACE]
+     * getCanonicalPath() 2 File [TRACE] isDirectory() 3 7 File [TRACE]
+     * File(File dir, String file) 7 RandomAccessFile [TRACE]
+     * RandomAccessFile(File path, String string) 7 RandomAccessFile [TRACE]
+     * close() 7 RandomAccessFile [TRACE] length() 14 File [TRACE] java.io.File
+     * getFile() 16 File [TRACE] getPath 22 RandomAccessFile [TRACE] read() 22
+     * RandomAccessFile [TRACE] seek()
+     * 
+     * 
+     * [nejoom:~/Documents/workspace/luceneOnCassandra]$ cat stats.read.bigger |
+     * cut -c 21- | sort | uniq -c | sort -n 1 File [TRACE] list() 1 uments 2
+     * File [TRACE] File(String canonicalPath 2 File [TRACE] exists() 2 File
+     * [TRACE] getCanonicalPath() 2 File [TRACE] isDirectory() 5 7 File [TRACE]
+     * File(File dir, String file) 7 RandomAccessFile [TRACE]
+     * RandomAccessFile(File path, String string) 7 RandomAccessFile [TRACE]
+     * close() 7 RandomAccessFile [TRACE] length() 14 File [TRACE] java.io.File
+     * getFile() 16 File [TRACE] getPath 24 RandomAccessFile [TRACE] read() 24
+     * RandomAccessFile [TRACE] seek()
+     */
+    private static Logger logger = LoggerFactory.getLogger(File.class);
 
     // TODO create 0 bytes file in the cassandra.
-    public FSFile(String canonicalPath)  {
+    public FSFile(String canonicalPath) {
         // TODO Auto-generated constructor stub
-        logger.info("File(String canonicalPath {}", canonicalPath);
+        logger.info("**** creating FSFile(String canonicalPath {})",
+                canonicalPath);
         thePrivateFile = new java.io.File(canonicalPath);
-//        thePrivateFile = new CassandraFile(canonicalPath);
     }
 
-    public FSFile(File dir, String file) {   // TODO create file in a directory.
+    public FSFile(File dir, String file) throws IOException {
+        logger.info("*** creating FSFile(String dir, file {}, {})",
+                dir.getCanonicalPath(), file);
+
+        // TODO create file in a directory.
         try {
-            logger.info("File(File {}, String file {} )", dir.getCanonicalPath(), file);
+            String cp = dir.getCanonicalPath();
+            thePrivateFile = new java.io.File(cp, file);
+            logger.info("creating File(File {}, String file {} )", cp, file);
+            logger.info("Did it get created? thePrivateFile.exists(): {}, {}",
+                    thePrivateFile.exists(), thePrivateFile.getCanonicalFile());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // public CassandraFile getFile() { // TODO not relaly imoprtant.
+    // logger.info("java.io.File getFile()");
+    // return thePrivateFile;
+    // }
+
+    public boolean exists() { // TODO check if the file is exists.
+        try {
+            logger.info("exists() {}, {}", thePrivateFile.exists(),
+                    thePrivateFile.getCanonicalFile());
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-      thePrivateFile = new java.io.File(file);
-//      thePrivateFile = new CassandraFile(dir.getFile(), file);
-    }
-    
-
-    public java.io.File getFile() { // TODO not relaly imoprtant.
-        logger.info("java.io.File getFile()");
-        return thePrivateFile;
-    }
-
-//    public CassandraFile getFile() { // TODO not relaly imoprtant.
-//        logger.info("java.io.File getFile()");
-//        return thePrivateFile;
-//    }
-
-    public boolean exists() { // TODO check if the file is exists.
-        logger.info("exists() {}", thePrivateFile.exists());
         return thePrivateFile.exists();
     }
 
@@ -155,52 +130,92 @@ private static Logger logger = LoggerFactory
         return thePrivateFile.isDirectory();
     }
 
-    public String[] list(java.io.FilenameFilter filenameFilter) { // TODO return a list of files.
-        String [] theArray = thePrivateFile.list(filenameFilter);
+    public String[] list(java.io.FilenameFilter filenameFilter) { // TODO return
+                                                                  // a list of
+                                                                  // files.
+        String[] theArray = thePrivateFile.list(filenameFilter);
         Arrays.sort(theArray);
         logger.info("list() {}", Arrays.toString(theArray));
         return theArray;
     }
 
-    public String getCanonicalPath() throws IOException{ // TODO get the path.
+    public String getCanonicalPath() throws IOException { // TODO get the path.
         logger.info("getCanonicalPath() {}", thePrivateFile.getCanonicalPath());
         return thePrivateFile.getCanonicalPath();
     }
 
-    public long length() {  // TODO length of the file.
+    public long length() { // TODO length of the file.
         logger.info("length() {}", thePrivateFile.length());
         return thePrivateFile.length();
     }
 
     public boolean delete() { // TODO delete the file.
-//        logger.info("delete() {}", thePrivateFile.delete());
+        // logger.info("delete() {}", thePrivateFile.delete());
         return thePrivateFile.delete();
     }
 
     public boolean mkdirs() { // TODO just return true.
-//        logger.info("mkdirs() {}", thePrivateFile.mkdirs());
+        // logger.info("mkdirs() {}", thePrivateFile.mkdirs());
         return thePrivateFile.mkdirs();
     }
 
     public String getPath() {
-        logger.info("getPath() {}", thePrivateFile.getPath());  // TODO pointer to the right file. like a lot of indices
+        logger.info("getPath() {}", thePrivateFile.getPath());
+        // TODO pointer to the right file. like a lot of indices
         return thePrivateFile.getPath();
     }
 
-
-    public long lastModified() {  // TODO set last modified.
-        logger.info("lastModified() {}", thePrivateFile.lastModified());  // TODO pointer to the right file. like a lot of indices
+    public long lastModified() { // TODO set last modified.
+        logger.info("lastModified() {}", thePrivateFile.lastModified());
+        // TODO pointer to the right file like a lot of indices
         return thePrivateFile.lastModified();
     }
 
     public boolean createNewFile() throws IOException {
-//        logger.info("createNewFile() {}", thePrivateFile.createNewFile());  // TODO pointer to the right file. like a lot of indices
-        return thePrivateFile.createNewFile();  // TODO create empty file.
+        // logger.info("createNewFile() {}", thePrivateFile.createNewFile()); //
+        // TODO pointer to the right file. like a lot of indices
+        return thePrivateFile.createNewFile(); // TODO create empty file.
     }
 
     public String getAbsolutePath() {
-        logger.info("getAbsolutePath() {}", thePrivateFile.getAbsolutePath());// TODO pointer to the right file. like a lot of indices
+        logger.info("getAbsolutePath() {}", thePrivateFile.getAbsolutePath());
+        // TODO pointer to the right file. like a lot of indices
         return thePrivateFile.getAbsolutePath(); // TODO get the path.
     }
 
+    public static int getDNCount = 0;
+    public static long getDNTime = 0;
+    @Override
+    public File get(File directory, String name) {
+        getDNCount++;
+        long ms = System.currentTimeMillis();
+
+        try {
+            return new FSFile(directory, name);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        getDNTime += System.currentTimeMillis() - ms;
+
+        return directory;
+    }
+
+    @Override
+    public File get(String canonicalPath) {
+        return new FSFile(canonicalPath);
+    }
+
+    @Override
+    public RandomAccessFile getRandomAccessFile(File fullFile,
+            String permissions) throws FileNotFoundException {
+        // TODO Auto-generated method stub
+        return new FSRandomAccessFile(fullFile, permissions);
+    }
+
+    // @Override
+    // public RandomAccessFile getRandomAccessFile(File fullFile,
+    // String permissions) throws FileNotFoundException {
+    // return new FSRandomAccessFile(fullFile, permissions);
+    // }
 }
