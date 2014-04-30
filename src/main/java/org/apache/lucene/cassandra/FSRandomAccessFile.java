3c05e4d6875002cf26fe40e0708a81c9be64e340
Jason Wee <peichieh@gmail.com>
2014-04-30 14:15:32 +0800

    OTHER-932 commit work done by eddie: reimplement FSRandomAccessFile

--- src/main/java/org/apache/lucene/cassandra/FSRandomAccessFile.java
+++ src/main/java/org/apache/lucene/cassandra/FSRandomAccessFile.java
@@ -29,12 +29,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /** @lucene.internal */
-public class FSRandomAccessFile  implements Serializable {
-//    CassandraRandomAccessFile thePrivateFile;
+public class FSRandomAccessFile implements RandomAccessFile, Serializable {
+    // CassandraRandomAccessFile thePrivateFile;
     java.io.RandomAccessFile thePrivateFile;
-    File currentFile = null;
-    
-    
+
+    // File currentFile = null;
+
     private static Logger logger = LoggerFactory
             .getLogger(RandomAccessFile.class);
 
@@ -42,12 +42,35 @@ public class FSRandomAccessFile  implements Serializable {
      * 
      */
     private static final long serialVersionUID = -526415350772290568L;
+    private String path;
 
-    public FSRandomAccessFile(File path, String string) throws FileNotFoundException {
-        logger.info("RandomAccessFile(File path, String string {} )", string);
-        currentFile = path;
-//        thePrivateFile = new CassandraRandomAccessFile(path.getFile(), string) ;
-        //thePrivateFile = new java.io.RandomAccessFile(path.getFile(), string) ;
+    public FSRandomAccessFile(File path, String permission)
+            throws FileNotFoundException {
+        try {
+            logger.info("RandomAccessFile(File path {}, permission {} )", path.getCanonicalPath(), permission);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        // currentFile = path;
+        // thePrivateFile = new CassandraRandomAccessFile(path.getFile(),
+        // string) ;
+        try {
+            try {
+                this.path = path.getCanonicalPath();
+                path.createNewFile();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            thePrivateFile =
+                    new java.io.RandomAccessFile(path.getCanonicalPath(),
+                            permission);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     public long length() throws IOException {
@@ -56,33 +79,46 @@ public class FSRandomAccessFile  implements Serializable {
     }
 
     public void close() throws IOException {
-        logger.info("close()");
+        logger.info("close() {}", path);
         thePrivateFile.close();
     }
 
-    public void write(byte[] b, int offset, int n) throws IOException {  // Writes n bytes from the specified byte array starting at offset off to this file.
+    public static int writeCount = 0;
+    public static long writeTime = 0;
+    public void write(byte[] b, int offset, int n) throws IOException { //
+        // Writes n bytes from the specified byte array starting at offset off
+        // to this fille
         logger.info("write() {}", Util.bytesToHex(b));
         logger.info("write() {}", offset);
         logger.info("write() {}", n);
+        long ms = System.currentTimeMillis();
+        writeCount++;
         thePrivateFile.write(b, offset, n);
-        
+        writeTime += System.currentTimeMillis() - ms;
+
     }
-    
-    public void seek(long pos) throws IOException { // Sets the file-pointer offset, measured from the beginning of this file, at which the next read or write occurs. The offset may be set beyond the end of the file. Setting the offset beyond the end of the file does not change the file length. The file length will change only by writing after the offset has been set beyond the end of the file.
+
+    public void seek(long pos) throws IOException {
+        // Sets the file-pointer offset, measured from the beginning of this
+        // file, at which the next read or write occurs. The offset may be set
+        // beyond the end of the file.
+        // Setting the offset beyond the end of the file does not change the
+        // file length. The file length will change only by writing after the
+        // offset has been set beyond the end of the file.
         logger.info("seek() {}", pos);
         thePrivateFile.seek(pos);
-        
+
     }
 
     public void setLength(long length) throws IOException { // http://docs.oracle.com/javase/7/docs/api/java/io/RandomAccessFile.html#setLength%28long%29
         logger.info("setLength() {}", length);
         thePrivateFile.setLength(length);
-        
+
     }
 
-    public int read(byte[] b, int i, int toRead) throws IOException { //http://docs.oracle.com/javase/7/docs/api/java/io/RandomAccessFile.html#read%28byte[],%20int,%20int%29
-        logger.info("read() {}", currentFile.getFile().getName());
-        int read = thePrivateFile.read(b,i,toRead);
+    public int read(byte[] b, int i, int toRead) throws IOException { // http://docs.oracle.com/javase/7/docs/api/java/io/RandomAccessFile.html#read%28byte[],%20int,%20int%29
+        // logger.info("read() {}", currentFile.getFile().getName());
+        int read = thePrivateFile.read(b, i, toRead);
         logger.info("read int {}", read);
         logger.info("Util.bytesToHex({})", Util.bytesToHex(b));
 
@@ -96,15 +132,15 @@ public class FSRandomAccessFile  implements Serializable {
 
     public void getFDsync() throws SyncFailedException, IOException {
         logger.info("getFDsync()");
-       thePrivateFile.getFD().sync(); // write into cassandra. // http://docs.oracle.com/javase/7/docs/api/java/io/FileDescriptor.html#sync()
-//        thePrivateFile.getFDsync();
+        thePrivateFile.getFD().sync(); // write into cassandra. //
+                                       // http://docs.oracle.com/javase/7/docs/api/java/io/FileDescriptor.html#sync()
+        // thePrivateFile.getFDsync();
     }
 
     public boolean getFDvalid() throws IOException {
-        logger.info("getFDvalid()"); // test if this file 
+        logger.info("getFDvalid()"); // test if this file
         return thePrivateFile.getFD().valid();
-//        return thePrivateFile.getFDvalid();
+        // return thePrivateFile.getFDvalid();
     }
-
 
 }
