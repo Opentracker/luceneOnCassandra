849024884be3b5e747b259f7b73eda7474673be1
Jason Wee <peichieh@gmail.com>
2014-05-12 19:14:24 +0800

    removed NativeFSLockFactory

--- src/main/java/org/apache/lucene/cassandra/FSDirectory.java
+++ src/main/java/org/apache/lucene/cassandra/FSDirectory.java
@@ -35,7 +35,6 @@ import org.apache.lucene.store.IOContext;
 import org.apache.lucene.store.IndexInput;
 import org.apache.lucene.store.IndexOutput;
 import org.apache.lucene.store.LockFactory;
-import org.apache.lucene.store.NativeFSLockFactory;
 import org.apache.lucene.store.NoSuchDirectoryException;
 
 import org.apache.lucene.util.ThreadInterruptedException;
