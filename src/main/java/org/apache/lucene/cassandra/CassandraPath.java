0fa838653543d685041ea3e38024be900e7bc864
Jason Wee <peichieh@gmail.com>
2014-05-13 15:22:58 +0800

    implemented method normalizeAndCheck, checkNotNul, normalize, encode when lucene trying to obtain lock file.

--- src/main/java/org/apache/lucene/cassandra/CassandraPath.java
+++ src/main/java/org/apache/lucene/cassandra/CassandraPath.java
@@ -2,31 +2,131 @@ package org.apache.lucene.cassandra;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.ref.SoftReference;
 import java.net.URI;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.CoderResult;
+import java.nio.charset.CodingErrorAction;
 import java.nio.file.FileSystem;
+import java.nio.file.InvalidPathException;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.WatchEvent.Kind;
 import java.nio.file.WatchEvent.Modifier;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
+import java.util.Arrays;
 import java.util.Iterator;
 
 public class CassandraPath implements Path  {
-
-    public CassandraPath(CassandraFileSystem cassandraFileSystem, String string) {
-        // TODO Auto-generated constructor stub
+    
+    private final CassandraFileSystem fs;
+    
+    private static ThreadLocal<SoftReference<CharsetEncoder>> encoder =
+            new ThreadLocal<SoftReference<CharsetEncoder>>();
+    
+    // internal representation
+    private final byte[] path;
+    
+    public CassandraPath(CassandraFileSystem fs, byte[] path) {
+        this.fs = fs;
+        this.path = path;
     }
 
-    public static String normalizeAndCheck(String dir) {
-        // TODO Auto-generated method stub
-        return null;
+    public CassandraPath(CassandraFileSystem fs, String input) {
+        // removes redundant slashes and checks for invalid characters
+        this(fs, encode(fs, normalizeAndCheck(input)));
+    }
+
+    public static String normalizeAndCheck(String input) {
+        int n = input.length();
+        char prevChar = 0;
+        for (int i = 0; i < n; i++) {
+            char c = input.charAt(i);
+            if ((c == '/') && (prevChar == '/'))
+                return normalize(input, n, i -1);
+            checkNotNul(input, c);
+            prevChar = c;
+        }
+        if (prevChar == '/')
+            return normalize(input, n, n - 1);
+        return input;
+    }
+    
+    private static void checkNotNul(String input, char c) {
+        if (c == '\u0000')
+            throw new InvalidPathException(input, "Nul character not allowed");
+    }
+    
+    private static String normalize(String input, int len, int off) {
+        if (len == 0)
+            return input;
+        int n = len;
+        while ((n > 0) && (input.charAt(n - 1) == '/')) n--;
+        if (n == 0)
+            return "/";
+        StringBuilder sb = new StringBuilder(input.length());
+        if (off > 0)
+            sb.append(input.substring(0, off));
+        char prevChar = 0;
+        for (int i=off; i < n; i++) {
+            char c = input.charAt(i);
+            if ((c == '/') && (prevChar == '/'))
+                continue;
+            checkNotNul(input, c);
+            sb.append(c);
+            prevChar = c;
+        }
+        return sb.toString();
+    }
+    
+    // encodes the given path-string into a sequence of bytes
+    private static byte[] encode(CassandraFileSystem fs, String input) {
+        SoftReference<CharsetEncoder> ref = encoder.get();
+        CharsetEncoder ce = (ref != null) ? ref.get() : null;
+        if (ce == null) {
+            ce = Charset.defaultCharset().newEncoder()
+                .onMalformedInput(CodingErrorAction.REPORT)
+                .onUnmappableCharacter(CodingErrorAction.REPORT);
+            encoder.set(new SoftReference<CharsetEncoder>(ce));
+        }
+
+        char[] ca = fs.normalizeNativePath(input.toCharArray());
+
+        // size output buffer for worse-case size
+        byte[] ba = new byte[(int)(ca.length * (double)ce.maxBytesPerChar())];
+
+        // encode
+        ByteBuffer bb = ByteBuffer.wrap(ba);
+        CharBuffer cb = CharBuffer.wrap(ca);
+        ce.reset();
+        CoderResult cr = ce.encode(cb, bb, true);
+        boolean error;
+        if (!cr.isUnderflow()) {
+            error = true;
+        } else {
+            cr = ce.flush(bb);
+            error = !cr.isUnderflow();
+        }
+        if (error) {
+            throw new InvalidPathException(input,
+                "Malformed input or input contains unmappable chacraters");
+        }
+
+        // trim result to actual length if required
+        int len = bb.position();
+        if (len != ba.length)
+            ba = Arrays.copyOf(ba, len);
+
+        return ba;
     }
 
     @Override
     public FileSystem getFileSystem() {
-        // TODO Auto-generated method stub
-        return null;
+        return fs;
     }
 
     @Override
