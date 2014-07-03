fa313ab06e5836b0a2f5cbdd22169dd5189e5fdc
Jason Wee <peichieh@gmail.com>
2014-07-03 19:33:01 +0800

    LEAD-260 initial implement method read(ByteBuffer) as this method is reference by project elasticsearchOnCassandra

--- src/main/java/org/apache/lucene/cassandra/nio/FileChannelImpl.java
+++ src/main/java/org/apache/lucene/cassandra/nio/FileChannelImpl.java
@@ -3,12 +3,16 @@ package org.apache.lucene.cassandra.nio;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
+import java.nio.channels.ClosedChannelException;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
+import java.nio.channels.NonReadableChannelException;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.channels.WritableByteChannel;
 
 import org.apache.lucene.cassandra.FileDescriptor;
+
+import sun.misc.IoTrace;
 
 public class FileChannelImpl extends FileChannel {
     
@@ -17,8 +21,7 @@ public class FileChannelImpl extends FileChannel {
     //private static final long allocationGranularity;
 
     // Used to make native read and write calls
-    // TODO Fix below;
-    //private final FileDispatcher nd;
+    private final FileDispatcher nd;
     
     // File descriptor
     private FileDescriptor fd;
@@ -31,37 +34,75 @@ public class FileChannelImpl extends FileChannel {
     // Required to prevent finalization of creating stream (immutable)
     private Object parent;
     
-    private FileChannelImpl(FileDescriptor fd, boolean readable,
+    // The path of the referenced file (null if the parent stream is created with a file descriptor)
+    private final String path;
+    
+    // Thread-safe set of IDs of native threads, for signalling
+    private final NativeThreadSet threads = new NativeThreadSet(2);
+    
+    // Lock for operations involving position and size
+    private final Object positionLock = new Object();
+    
+    private FileChannelImpl(FileDescriptor fd, String path, boolean readable,
             boolean writable, boolean append, Object parent) {
         this.fd = fd;
         this.readable = readable;
         this.writable = writable;
         this.append = append;
         this.parent = parent;
-        // TODO Fix below;
-       // this.nd = new FileDispatcherImpl(append);
+        this.path = path;
+        this.nd = new FileDispatcherImpl(append);
     }
 
     // Used by CassandraRandomAccessFile.getChannel()
-    public static FileChannel open(FileDescriptor fd,
+    public static FileChannel open(FileDescriptor fd, String path,
                                    boolean readable, boolean writable,
                                    Object parent)
     {
-        return new FileChannelImpl(fd, readable, writable, false, parent);
+        return new FileChannelImpl(fd, path, readable, writable, false, parent);
     }
 
     // Used by FileOutputStream.getChannel
-    public static FileChannel open(FileDescriptor fd,
+    public static FileChannel open(FileDescriptor fd, String path,
                                    boolean readable, boolean writable,
                                    boolean append, Object parent)
     {
-        return new FileChannelImpl(fd, readable, writable, append, parent);
+        return new FileChannelImpl(fd, path, readable, writable, append, parent);
     }
 
+    /**
+     * Reads a sequence of bytes from this channel into the given buffer.
+     *
+     * <p> Bytes are read starting at this channel's current file position, and
+     * then the file position is updated with the number of bytes actually
+     * read.  Otherwise this method behaves exactly as specified in the {@link
+     * ReadableByteChannel} interface. </p>
+     */
     @Override
     public int read(ByteBuffer dst) throws IOException {
-        // TODO implement
-        return 0;
+        ensureOpen();
+        if (!readable)
+            throw new NonReadableChannelException();
+        synchronized (positionLock) {
+            int n = 0;
+            int ti = -1;
+            Object traceContext = IoTrace.fileReadBegin(path);
+            try {
+                begin();
+                ti = threads.add();
+                if (!isOpen())
+                    return 0;
+                do {
+                    n = IOUtil.read(fd, dst, -1, nd);
+                } while ((n == IOStatus.INTERRUPTED) && isOpen());
+                return IOStatus.normalize(n);
+            } finally  {
+                threads.remove(ti);
+                IoTrace.fileReadEnd(traceContext, n > 0 ? n : 0);
+                end(n > 0);
+                assert IOStatus.check(n);
+            }
+        }
     }
 
     @Override
@@ -174,6 +215,11 @@ public class FileChannelImpl extends FileChannel {
     protected void implCloseChannel() throws IOException {
         // TODO Auto-generated method stub
         
+    }
+    
+    private void ensureOpen() throws IOException {
+        if (!isOpen())
+            throw new ClosedChannelException();
     }
 
 }
