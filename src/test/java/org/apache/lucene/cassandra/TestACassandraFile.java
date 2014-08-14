620ec9c247626c3fd2ea3f6d3ac13d7059efdeba
Jason Wee <peichieh@gmail.com>
2014-08-14 09:23:26 +0800

    LEAD-451 rewrite test case for testListFiles()

--- src/test/java/org/apache/lucene/cassandra/TestACassandraFile.java
+++ src/test/java/org/apache/lucene/cassandra/TestACassandraFile.java
@@ -17,7 +17,7 @@ import org.junit.Test;
 import net.opentracker.test.OpentrackerTestBase;
 
 public class TestACassandraFile extends OpentrackerTestBase {
-    
+
     CassandraClient client = null;
 
     @BeforeClass
@@ -31,11 +31,13 @@ public class TestACassandraFile extends OpentrackerTestBase {
     @Before
     public void setUp() throws Exception {
         try {
-            client = new CassandraClient(cassandraHost, rpcPort, frameMode, keyspace, columnFamily, blockSize);
+            client =
+                    new CassandraClient(cassandraHost, rpcPort, frameMode,
+                            keyspace, columnFamily, blockSize);
         } catch (IOException e) {
             fail("exception is not expected");
         }
-        
+
         assertNotNull(client);
     }
 
@@ -46,88 +48,94 @@ public class TestACassandraFile extends OpentrackerTestBase {
 
     @Test
     public void testRead() {
-        
+
         try {
-            ACassandraFile writeFile = new ACassandraFile("/", "removeMe.txt", IOContext.DEFAULT, true, keyspace, columnFamily, 4);
-            
+            ACassandraFile writeFile =
+                    new ACassandraFile("/", "removeMe.txt", IOContext.DEFAULT,
+                            true, keyspace, columnFamily, 4);
+
             // C B A
-            byte[] b = {70, 69, 68, 67, 66, 65};
+            byte[] b = { 70, 69, 68, 67, 66, 65 };
             // in cassandra, stored as 46454443
-            //                                        4241
+            // 4241
             writeFile.write(b, 0, b.length);
             writeFile.close();
-            
-            ACassandraFile readFile = new ACassandraFile("/", "removeMe.txt", IOContext.READ, true, keyspace, columnFamily, 4);
+
+            ACassandraFile readFile =
+                    new ACassandraFile("/", "removeMe.txt", IOContext.READ,
+                            true, keyspace, columnFamily, 4);
             int value = readFile.read();
             assertEquals(70, value);
-            
+
             value = readFile.read();
             assertEquals(69, value);
-            
+
             value = readFile.read();
             assertEquals(68, value);
-            
+
             value = readFile.read();
             assertEquals(67, value);
-            
+
             value = readFile.read();
             assertEquals(66, value);
-            
+
             value = readFile.read();
             assertEquals(65, value);
-            
+
             value = readFile.read();
             assertEquals(-1, value);
             readFile.close();
-            
+
         } catch (IOException e) {
             e.printStackTrace();
             fail("fail is not expected");
         }
-        
+
     }
-    
+
     @Test
     public void testRead1() {
         try {
-            ACassandraFile writeFile = new ACassandraFile("/", "removeMe.txt", IOContext.DEFAULT, true, keyspace, columnFamily, blockSize);
+            ACassandraFile writeFile =
+                    new ACassandraFile("/", "removeMe.txt", IOContext.DEFAULT,
+                            true, keyspace, columnFamily, blockSize);
             writeFile.write(67, true);
             writeFile.write(66, true);
             writeFile.write(65, true);
             writeFile.close();
-            // in cassandra store as 
-            // block-0 43 
+            // in cassandra store as
+            // block-0 43
             // block-1 42
             // block-2 41
-            
-            ACassandraFile readFile = new ACassandraFile("/", "removeMe.txt", IOContext.READ, true, keyspace, columnFamily, 4);
+
+            ACassandraFile readFile =
+                    new ACassandraFile("/", "removeMe.txt", IOContext.READ,
+                            true, keyspace, columnFamily, 4);
             int value = readFile.read();
             assertEquals(67, value);
-            
+
             value = readFile.read();
             assertEquals(66, value);
-            
+
             value = readFile.read();
             assertEquals(65, value);
-            
+
             value = readFile.read();
             assertEquals(-1, value);
             readFile.close();
-            
+
         } catch (Exception e) {
             e.printStackTrace();
             fail("fail is not expected");
         }
-        
+
     }
 
-    //   TODO File[] indicesList = indicesLocation.listFiles(); is wrong , need to rewrite
     @Test
     public void  testListFiles() {
 
         // ---test public File[] listFiles() ---
         try {
-            System.out.println("test 0");
             // prepare some data
             ACassandraFile writeFile = new ACassandraFile("/", "removeMe.txt", IOContext.DEFAULT, true, keyspace, columnFamily, blockSize);
             writeFile.write(67, true);
@@ -150,31 +158,35 @@ public class TestACassandraFile extends OpentrackerTestBase {
             writeFile.write(65, true);
             writeFile.close();
 
-            System.out.println("test 1");
             // test.
             ACassandraFile file = new ACassandraFile("/test", "", IOContext.READ, true, keyspace, columnFamily, blockSize);
             File[] files = file.listFiles();
             file.close();
-
-            System.out.println("test 2");
-            assertEquals(3, files.length);
-            System.out.println(Arrays.asList(files).contains(new ACassandraFile("/test", "removeMe.txt", IOContext.READ, true, keyspace, columnFamily, blockSize)));
-            System.out.println(Arrays.asList(files).contains(new ACassandraFile("/test", "baz.txt", IOContext.READ, true, keyspace, columnFamily, blockSize)));
-            System.out.println(Arrays.asList(files).contains(new ACassandraFile("/test", "bar.txt", IOContext.READ, true, keyspace, columnFamily, blockSize)));
-            System.out.println("test 3");
-            // in cassandra, it is /removeMe.txt;
-            //assertEquals("removeMe.txt", files[0].getName());
-            //assertEquals("/test/removeMe.txt", files[0].getAbsolutePath());
             
-            //assertEquals("/removeMe.txt", files[1].getName());
-            //assertEquals("/removeMe.txt", files[1].getAbsolutePath());
+            assertEquals(3, files.length);            
+            assertTrue(Arrays.toString(files), Arrays.asList(files).contains(new ACassandraFile("/test", "removeMe.txt", IOContext.READ, true, keyspace, columnFamily, blockSize)));
+            assertTrue(Arrays.toString(files), Arrays.asList(files).contains(new ACassandraFile("/test", "baz.txt", IOContext.READ, true, keyspace, columnFamily, blockSize)));
+            assertTrue(Arrays.toString(files), Arrays.asList(files).contains(new ACassandraFile("/test", "bar.txt", IOContext.READ, true, keyspace, columnFamily, blockSize)));
+            
+            file = new ACassandraFile("/", "", IOContext.READ, true, keyspace, columnFamily, blockSize);
+            files = file.listFiles();
+            file.close();
+            
+            assertEquals(1, files.length);            
+            assertTrue(Arrays.toString(files), Arrays.asList(files).contains(new ACassandraFile("/", "removeMe.txt", IOContext.READ, true, keyspace, columnFamily, blockSize)));
 
         } catch (Exception e) {
             e.printStackTrace();
+        } finally  {
+            try {
+                client.truncate(columnFamily);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
         // ---test public File[] listFiles() ---
 
-        /*
         // ---test public File[] listFiles(CassandraFileFilter filter) ---
         try {
             // prepare some data
@@ -189,7 +201,7 @@ public class TestACassandraFile extends OpentrackerTestBase {
             File[] files = file.listFiles(null);
             file.close();
 
-            assertTrue(files.length == 1);
+            assertTrue(String.valueOf(files.length), files.length == 1);
             // in cassandra, it is /removeMe.txt;
             assertEquals("/removeMe.txt", files[0].getName());
 
@@ -225,39 +237,39 @@ public class TestACassandraFile extends OpentrackerTestBase {
             e.printStackTrace();
         }
         // ---test public File[] listFiles(CassandraFileFilter filter) ---
-         
-         */
     }
 
     /**
-     * Note: This default to always lucene0 and index0, which is not good. Putting in
-     * here and to improve and change in the future.
+     * Note: This default to always lucene0 and index0, which is not good.
+     * Putting in here and to improve and change in the future.
      */
     @Test
     public void testACassandraFile() {
-        
+
         CassandraClient lucene0 = null;
-        
+
         // == Test ACassandraFile(File parent, String child) ==
         try {
-            lucene0 = new CassandraClient(cassandraHost, rpcPort, frameMode, "lucene0", "index0", blockSize);
-            
+            lucene0 =
+                    new CassandraClient(cassandraHost, rpcPort, frameMode,
+                            "lucene0", "index0", blockSize);
+
             // prepare data
             String child = "123";
             ACassandraFile parent = new ACassandraFile("/test/removeMe.txt");
-           
+
             ACassandraFile file = new ACassandraFile(parent, child);
             file.close();
-            
+
             List<byte[]> column = new ArrayList<byte[]>();
             column.add("DESCRIPTOR".getBytes());
-            
+
             byte[][] arrays = lucene0.getKeys(column, 1024);
-            
+
             assertEquals(2, arrays.length);
             assertEquals("/test/removeMe.txt", new String(arrays[0]));
             assertEquals("/test/removeMe.txt/123", new String(arrays[1]));
-            
+
         } catch (IOException e) {
             e.printStackTrace();
             fail("exception is not expected");
@@ -271,14 +283,16 @@ public class TestACassandraFile extends OpentrackerTestBase {
             }
         }
         // == Test ACassandraFile(File parent, String child) ==
-        
 
-        // == Test ACassandraFile(String canonicalPath)  ==
+        // == Test ACassandraFile(String canonicalPath) ==
         try {
-            lucene0 = new CassandraClient(cassandraHost, rpcPort, frameMode, "lucene0", "index0", blockSize);
-            
+            lucene0 =
+                    new CassandraClient(cassandraHost, rpcPort, frameMode,
+                            "lucene0", "index0", blockSize);
+
             // prepare data
-            ACassandraFile testFile = new ACassandraFile("/test/dummy/removeMe.txt");
+            ACassandraFile testFile =
+                    new ACassandraFile("/test/dummy/removeMe.txt");
             testFile.close();
             assertEquals("/test/dummy", testFile.getParent(true));
             assertEquals("/test/dummy/removeMe.txt", testFile.getName());
@@ -294,18 +308,24 @@ public class TestACassandraFile extends OpentrackerTestBase {
                 }
             }
         }
-        // == Test ACassandraFile(String canonicalPath)  ==
-        
-        
-        // == ACassandraFile(String directory, String name, IOContext mode, boolean frameMode, String keyspace, String columnFamily, int blockSize)  ==
+        // == Test ACassandraFile(String canonicalPath) ==
+
+        // == ACassandraFile(String directory, String name, IOContext mode,
+        // boolean frameMode, String keyspace, String columnFamily, int
+        // blockSize) ==
 
         // prepare data
-        ACassandraFile testFile = new ACassandraFile("/test/dummy/", "removeMeMe.txt", IOContext.DEFAULT, true, keyspace, columnFamily, blockSize);
+        ACassandraFile testFile =
+                new ACassandraFile("/test/dummy/", "removeMeMe.txt",
+                        IOContext.DEFAULT, true, keyspace, columnFamily,
+                        blockSize);
         testFile.close();
         assertEquals("/test/dummy", testFile.getParent(true));
         assertEquals("/test/dummy/removeMeMe.txt", testFile.getName());
 
-        // == ACassandraFile(String directory, String name, IOContext mode, boolean frameMode, String keyspace, String columnFamily, int blockSize)  ==
+        // == ACassandraFile(String directory, String name, IOContext mode,
+        // boolean frameMode, String keyspace, String columnFamily, int
+        // blockSize) ==
 
     }
 }
